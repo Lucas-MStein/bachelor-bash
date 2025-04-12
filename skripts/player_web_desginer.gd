1683a289d6f1fa205bdae7a125aa5977104a7457
zydapelo <linda.rein@online.de>
2025-04-12 17:41:45 +0200

    flexible player animation

--- skripts/player_web_desginer.gd
+++ skripts/player_web_desginer.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
+var double_jump_used = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,8 +12,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif not double_jump_used:
+			velocity.y = JUMP_VELOCITY
+			double_jump_used = true
+	
+	if is_on_floor():
+		double_jump_used = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
