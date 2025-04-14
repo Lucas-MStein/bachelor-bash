f3e2dc633bba7fc1d078f4ae87e2a908aac34963
Lucas-Maurice Stein <lms@koempf24.de>
2025-04-14 12:22:09 +0200

    menu and character select

--- main_menu.gd
+++ main_menu.gd
@@ -1,16 +1,35 @@
 extends Control
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Level1.tscn")
+@onready var label_node: Label = get_node("Label")
 
+func _ready() -> void:
+	# Entwickler-Test: Spielstand nur beim ersten Start in Debug löschen
+	if OS.is_debug_build() and Global.first_run:
+		Global.character = ""
+		Global.level = 1
+		var dir := DirAccess.open("user://")
+		if dir.file_exists("savegame.save"):
+			dir.remove("savegame.save")
+		Global.first_run = false  # Wichtig: Nur einmal löschen
+
+	# Savegame laden (falls vorhanden)
+	Global.load_game()
+
+	# Begrüßungstext setzen
+	if Global.character != "":
+		$Label.text = "Willkommen zurück, " + Global.character + "!"
+	else:
+		$Label.text = "Bachelor Bash"
+
+func _on_start_pressed() -> void:
+	if Global.character == "":
+		label_node.text = "Bitte wähle zuerst einen Charakter!"
+	else:
+		Global.save_game()
+		get_tree().change_scene_to_file("res://Level1.tscn")
 
 func _on_character_pressed() -> void:
-	print("Character pressed")
-
-
-func _on_options_pressed() -> void:
-	print("Options pressed")
-
+	get_tree().change_scene_to_file("res://character_select_menu.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
