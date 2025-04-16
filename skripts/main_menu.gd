561cef53980266051cb2c8dd4f8f47b224657bc1
zydapelo <linda.rein@online.de>
2025-04-16 11:00:32 +0200

    Connection between classroom and game

--- skripts/main_menu.gd
+++ skripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Level1.tscn")
+	get_tree().change_scene_to_file("res://scences/testing.tscn")
 
 
 func _on_character_pressed() -> void:
