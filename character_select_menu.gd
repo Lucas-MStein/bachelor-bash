560d755b376d2209f705c379b933bd3b49e2f596
Lucas-Maurice Stein <lms@koempf24.de>
2025-04-24 10:14:31 +0200

    new work branch
    # Conflicts:
    #	.godot/editor/editor_layout.cfg
    #	.godot/editor/filesystem_update4
    #	.godot/editor/project_metadata.cfg
    #	.godot/editor/script_editor_cache.cfg

---
+++ character_select_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/HBoxContainer/Char1Button.pressed.connect(func(): _select_character("Projekt-Manager"))
+	$VBoxContainer/HBoxContainer/Char2Button.pressed.connect(func(): _select_character("Web-Designer"))
+	$VBoxContainer/HBoxContainer/Char3Button.pressed.connect(func(): _select_character("Code-Master"))
+
+func _select_character(name: String):
+	Global.character = name
+	$VBoxContainer/InfoLabel.text = "Ausgewählt: " + name
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://main_menu.tscn")
