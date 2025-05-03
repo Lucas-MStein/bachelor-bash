c6e04cc8237f5dc1dc8e1b442dcdf7491ca0963f
Carla <carla.erb@web.de>
2025-05-03 21:02:02 +0200

    player and level connected

--- skripts/main_menu.gd
+++ skripts/main_menu.gd
@@ -27,7 +27,7 @@ func _ready() -> void:
 
 func _on_start_pressed() -> void:
 	if Global.character == "":
-		label_node.text = "Bitte wähle zuerst einen Charakter!"
+		$Label2.text = "Bitte wähle zuerst einen Charakter!"
 	else:
 		Global.save_game()
 		get_tree().change_scene_to_file("res://scenes/Level_1.tscn")
