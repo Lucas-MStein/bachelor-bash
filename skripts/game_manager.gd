a408ffddfa0723d1539db4d95a1bd4985dfd2135
zydapelo <linda.rein@online.de>
2025-05-05 19:45:03 +0200

    creditpoints und hearts

--- skripts/game_manager.gd
+++ skripts/game_manager.gd
@@ -1,13 +1,27 @@
 extends Node2D
 
 var coins = 0
+var heart = 3
 
-@onready var coin_label: Label = $CanvasLayer/CoinLabel
-@onready var coin_container: GridContainer = $CanvasLayer/CoinContainer
+@onready var coin_container = get_node("CanvasLayer/CoinContainer")
+
+@onready var heart_container = get_node("CanvasLayer/HeartContainer")
+
+@export var active_heart: Texture
+@export var inactive_heart: Texture
+
 
 func add_coin():
-	coin_label.text = "Coins: " + str(coins)
 	if coins < 3:
 		var coin_icon = coin_container.get_child(coins)
 		coin_icon.visible = true
 		coins += 1
+		
+func damage_heart():
+	if heart > 0:
+		heart -= 1
+		var heart_icon = heart_container.get_child(heart)
+		if heart_icon is TextureRect:
+			heart_icon.texture = inactive_heart
+	else:
+		print("Spieler hat keine Leben mehr")
