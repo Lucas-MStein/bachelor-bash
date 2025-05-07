4e8e6810e9ce3064b6f58c208d8bac6b8284260d
zydapelo <linda.rein@online.de>
2025-05-07 17:27:01 +0200

    update

--- skripts/main_menu.gd
+++ skripts/main_menu.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	GameManager.update_ui_visibility()
 	MusicPlayer.is_active = true
 	
-	music_toggle.focus_mode = Control.FOCUS_NONE
+	#music_toggle.focus_mode = Control.FOCUS_NONE
 	music_toggle.button_pressed = !MusicPlayer.music_enabled
 	music_toggle.pressed.connect(_on_music_toggle_pressed)
 	# Entwickler-Test: Speicherstand löschen beim ersten Start (Debug-Modus)
